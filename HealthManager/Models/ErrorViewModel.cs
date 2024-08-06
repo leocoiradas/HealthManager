b1f1f38750c2677ee1f25166808d5cbf43c42e19
Alan Coiradas <coiradasalan@gmail.com>
2024-08-06 11:07:22 -0300

    Agregar archivos de proyecto.

---
+++ HealthManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HealthManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
