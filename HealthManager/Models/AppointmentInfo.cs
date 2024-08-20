6fc512d1b8c9d5be16c11e44b903c7b08f506aec
Alan Coiradas <coiradasalan@gmail.com>
2024-08-20 15:15:43 -0300

    Creación de AppointmentInfo

---
+++ HealthManager/Models/AppointmentInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HealthManager.Models;
+
+public partial class AppointmentInfo
+{
+    public int Id { get; set; }
+
+    public int DoctorId { get; set; }
+
+    public TimeOnly WorkingHoursStart { get; set; }
+
+    public TimeOnly WorkingHoursEnd { get; set; }
+
+    public TimeOnly ConsultationDuration { get; set; }
+
+    public int? WorkingDaysId { get; set; }
+
+    public virtual Doctor Doctor { get; set; } = null!;
+
+    public virtual WorkingDay? WorkingDays { get; set; }
+}
