98fcecb5585927d34177cddb7809b87250fe5403
Alan Coiradas <coiradasalan@gmail.com>
2024-08-20 15:16:16 -0300

    Creación de modelo MedicalRegister

---
+++ HealthManager/Models/MedicalRegister.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HealthManager.Models;
+
+public partial class MedicalRegister
+{
+    public Guid RegisterId { get; set; }
+
+    public int DoctorId { get; set; }
+
+    public int PatientId { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Resume { get; set; } = null!;
+
+    public virtual Doctor Doctor { get; set; } = null!;
+
+    public virtual Patient Patient { get; set; } = null!;
+}
