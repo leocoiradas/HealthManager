de36936b094bd0cc5872f27d52e1e0730900d43f
Alan Coiradas <coiradasalan@gmail.com>
2024-08-20 15:16:30 -0300

    Creación de modelo Patient

---
+++ HealthManager/Models/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HealthManager.Models;
+
+public partial class Patient
+{
+    public int PatientId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public DateOnly Birthdate { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int Dni { get; set; }
+
+    public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+    public virtual ICollection<MedicalRegister> MedicalRegisters { get; set; } = new List<MedicalRegister>();
+}
