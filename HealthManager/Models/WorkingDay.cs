51167cb0fe6aecf66e0fe27cb247a5983ed5266c
Alan Coiradas <coiradasalan@gmail.com>
2024-08-20 15:18:26 -0300

    Creación de modelo WorkingDay
    Nota: Este modelo representa los días en los que un médico atiende en la clínica, representados por un valor booleano en cada día, que indica mediante su valor de verdad si ése día el médico referenciado en la tabla atiende o no.

---
+++ HealthManager/Models/WorkingDay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HealthManager.Models;
+
+public partial class WorkingDay
+{
+    public int WorkingDaysId { get; set; }
+
+    public int? DoctorId { get; set; }
+
+    public bool? Monday { get; set; }
+
+    public bool? Tuesday { get; set; }
+
+    public bool? Wednesday { get; set; }
+
+    public bool? Thursday { get; set; }
+
+    public bool? Friday { get; set; }
+
+    public bool? Saturday { get; set; }
+
+    public bool? Sunday { get; set; }
+
+    public virtual ICollection<AppointmentInfo> AppointmentInfos { get; set; } = new List<AppointmentInfo>();
+
+    public virtual Doctor? Doctor { get; set; }
+}
