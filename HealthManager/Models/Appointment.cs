9da501eace7bfcf9538d66659d883f99d050c8c5
Alan Coiradas <coiradasalan@gmail.com>
2024-08-20 15:15:00 -0300

    Creación de clase Appointment

---
+++ HealthManager/Models/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HealthManager.Models;
+
+public partial class Appointment
+{
+    public Guid AppointmentId { get; set; }
+
+    public DateOnly AppointmentDate { get; set; }
+
+    public TimeOnly AppointmentHour { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public int PatientId { get; set; }
+
+    public int DoctorId { get; set; }
+
+    public virtual Doctor Doctor { get; set; } = null!;
+
+    public virtual Patient Patient { get; set; } = null!;
+}
