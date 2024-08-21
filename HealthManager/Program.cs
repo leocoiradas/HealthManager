7d8fb853c5800d8163217854caa6376f16f6a45f
Alan Coiradas <coiradasalan@gmail.com>
2024-08-21 19:09:21 -0300

    Program.cs: Añadida configuración de JWT + Cookies

--- HealthManager/Program.cs
+++ HealthManager/Program.cs
@@ -1,5 +1,8 @@
 using HealthManager.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,42 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<HealthManagerContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("HealthManager"));
+});
+
+//Configuración de autenticación
+
+var tokenKey = builder.Configuration.GetSection("JWT").GetSection("secret-key").ToString();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddCookie(c =>
+    {
+        c.Cookie.Name = "Token";
+    })
+.AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = false;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(tokenKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+    };
+    config.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            context.Token = context.Request.Cookies["Token"];
+            return Task.CompletedTask;
+        }
+    };
 });
 
 var app = builder.Build();
@@ -25,6 +64,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthorization();
 
 app.UseAuthorization();
 
