fd0e4b14aa04a964b9c5170155b61599c55cdcff
Alan Coiradas <coiradasalan@gmail.com>
2024-08-22 22:03:17 -0300

    Patient.cs:  Añadidos criterios de validación al modelo

--- HealthManager/Models/Patient.cs
+++ HealthManager/Models/Patient.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HealthManager.Models;
 
 public partial class Patient
 {
     public int PatientId { get; set; }
-
+    [Required]
+    [MaxLength(100)]
+    [MinLength(1)]
     public string Name { get; set; } = null!;
-
+    [MaxLength(100)]
+    [MinLength(1)]
     public string Surname { get; set; } = null!;
-
+    [Required]
     public DateOnly Birthdate { get; set; }
-
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = null!;
-
+    [Required, MinLength(12), RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$")]
     public string Password { get; set; } = null!;
-
+    [Required]
     public int Dni { get; set; }
 
     public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
