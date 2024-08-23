5656d20f1d5f9bfddca8d77f81b0f4e9efe20f50
Alan Coiradas <coiradasalan@gmail.com>
2024-08-22 22:03:42 -0300

    MedicalRegister:  Añadidos criterios de validación al modelo

--- HealthManager/Models/MedicalRegister.cs
+++ HealthManager/Models/MedicalRegister.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HealthManager.Models;
 
@@ -10,11 +11,13 @@ public partial class MedicalRegister
     public int DoctorId { get; set; }
 
     public int PatientId { get; set; }
-
+    [Required]
     public DateTime Date { get; set; }
 
+    [Required]
     public string Title { get; set; } = null!;
 
+    [Required]
     public string Resume { get; set; } = null!;
 
     public virtual Doctor Doctor { get; set; } = null!;
