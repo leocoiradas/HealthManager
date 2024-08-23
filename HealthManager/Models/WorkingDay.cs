048559088a96b613936b7e7edea34b3ae65d9c04
Alan Coiradas <coiradasalan@gmail.com>
2024-08-22 22:03:29 -0300

    WorkingDay:  Añadidos criterios de validación al modelo

--- HealthManager/Models/WorkingDay.cs
+++ HealthManager/Models/WorkingDay.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HealthManager.Models;
 
@@ -9,18 +10,25 @@ public partial class WorkingDay
 
     public int? DoctorId { get; set; }
 
+    [Required]
     public bool? Monday { get; set; }
 
+    [Required]
     public bool? Tuesday { get; set; }
 
+    [Required]
     public bool? Wednesday { get; set; }
 
+    [Required]
     public bool? Thursday { get; set; }
 
+    [Required]
     public bool? Friday { get; set; }
 
+    [Required]
     public bool? Saturday { get; set; }
 
+    [Required]
     public bool? Sunday { get; set; }
 
     public virtual ICollection<AppointmentInfo> AppointmentInfos { get; set; } = new List<AppointmentInfo>();
