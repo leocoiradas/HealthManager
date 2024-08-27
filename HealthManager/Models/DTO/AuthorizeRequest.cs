e9693173212c7aa628587cc876753a3ce4c0b24f
Alan Coiradas <coiradasalan@gmail.com>
2024-08-26 23:35:33 -0300

    AuthorizeRequest: Añadidas validaciones al modelo de AuthorizeRequest

--- HealthManager/Models/DTO/AuthorizeRequest.cs
+++ HealthManager/Models/DTO/AuthorizeRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace HealthManager.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthManager.Models.DTO
 {
     public class AuthorizeRequest
     {
+        [Required, EmailAddress]
         public string email {  get; set; }
+        [Required, MinLength(12) ,RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$")]
         public string password { get; set; }
     }
 }
