5af53e21eda02ed3adc43965e9bc540a1dda1ef2
Alan Coiradas <coiradasalan@gmail.com>
2024-08-28 22:18:15 -0300

    Creación de Service de citas médicas con métodos como CreateAppointments
    Se creó esta clase para almacenar los métodos encargados de gestionar las peticiones relacionadas a las citas médicas.
    De momento se está trabajando en el método CreateAppointments cuya idea es crear los perfiles o registros de citas médicas para que estos estén listos para ser solicitados por los pacientes.

---
+++ HealthManager/Services/Appointments/AppointmentsService.cs
@@ -0,0 +1,51 @@
+﻿using HealthManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthManager.Services.Appointments
+{
+    public class AppointmentsService : IAppointments
+    {
+        private readonly HealthManagerContext _context;
+
+        public AppointmentsService(HealthManagerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> CreateAppointments()
+        {
+            int currentYear = DateTime.Now.Year;
+            List<Doctor> doctorsCollection = _context.Doctors
+                .Include(d => d.AppointmentInfos)
+                .Include(d => d.WorkingDays)
+                .ToList();
+            foreach(Doctor doctor in doctorsCollection)
+            {
+                int currentMonth = DateTime.Now.Month;
+                int daysInMonth = System.DateTime.DaysInMonth(currentYear, currentMonth);
+
+                List<WorkingDay> doctorWorkingDays = doctor.WorkingDays.ToList();
+                for (int i=1; i<=daysInMonth; i++)
+                {
+                    int currentDay = i;
+
+                }
+            }
+        }
+
+        public async Task<Appointment> GetAvailableAppointments()
+        {
+            int month = 1;
+            int currentMonth = DateTime.Now.Month;
+            int currentYear = DateTime.Now.Year;
+            List<Appointment> appointments = _context.Appointments
+                .Where(x => x.AppointmentDate.Month == currentMonth && x.AppointmentDate.Year == currentYear)
+                .ToList();
+
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
