96707fd89ecd301aa1789ab88a96a14f2d3258cc
Alan Coiradas <coiradasalan@gmail.com>
2024-08-28 22:15:12 -0300

    Creado controlador de citas médicas

---
+++ HealthManager/Controllers/AppointmentController.cs
@@ -0,0 +1,23 @@
+﻿using HealthManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthManager.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateAppointment(Appointment newAppointment)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
