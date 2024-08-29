b189b709ed941953aac8d02f4cd36209cbdfe408
Alan Coiradas <coiradasalan@gmail.com>
2024-08-28 22:15:59 -0300

    Creada interfaz encargada de declarar métodos de citas médicas

---
+++ HealthManager/Services/Appointments/IAppointments.cs
@@ -0,0 +1,13 @@
+﻿using HealthManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthManager.Services.Appointments
+{
+    public interface IAppointments
+    {
+        public Task<Appointment> GetAvailableAppointments();
+
+        public Task<IActionResult> CreateAppointments();
+
+    }
+}
