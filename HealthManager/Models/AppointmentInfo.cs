b2329d66d5e5bad2d6dc8d631c63949cd792b35c
Alan Coiradas <coiradasalan@gmail.com>
2024-09-10 17:41:20 -0300

    AppointmentInfo: Añadida propiedad ForeignKey a DoctorId que apunta a la tabla "Doctor"

--- HealthManager/Models/AppointmentInfo.cs
+++ HealthManager/Models/AppointmentInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HealthManager.Models;
 
@@ -8,6 +9,7 @@ public partial class AppointmentInfo
 {
     public int Id { get; set; }
 
+    [ForeignKey("Doctor")]
     public int DoctorId { get; set; }
     [Required]
     public TimeOnly WorkingHoursStart { get; set; }
@@ -18,6 +20,7 @@ public partial class AppointmentInfo
 
     public int? WorkingDaysId { get; set; }
 
+    
     public virtual Doctor Doctor { get; set; } = null!;
 
     public virtual WorkingDay? WorkingDays { get; set; }
