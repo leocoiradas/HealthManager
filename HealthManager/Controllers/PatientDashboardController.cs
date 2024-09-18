80457111974116dd81cb019060675e1788e2d4cb
Alan Coiradas <coiradasalan@gmail.com>
2024-09-18 13:14:48 -0300

    Creación del controlador del dashboard de paciente

---
+++ HealthManager/Controllers/PatientDashboardController.cs
@@ -0,0 +1,50 @@
+﻿using HealthManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthManager.Controllers
+{
+    public class PatientDashboardController : Controller
+    {
+        private readonly HealthManagerContext _dbcontext;
+
+        public PatientDashboardController(HealthManagerContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task <IActionResult> MyAppointments()
+        {
+            /* var userCookie = Request.Cookies["User"];
+
+             if (string.IsNullOrEmpty(userCookie))
+             {
+                  return RedirectToAction("Login", "Authorize");
+             }
+
+
+             if (!int.TryParse(userCookie, out int patientId))
+             {
+                 return BadRequest("Invalid cookie value. There is no cookie.");
+             }
+
+             var patientAppointments = await _dbcontext.Appointments
+                 .Where(p => p.PatientId == patientId && p.Status=="Reserved")
+                 .ToListAsync();*/
+            var patientAppointments = await _dbcontext.Appointments
+                 .Where(p => p.Status == "Reserved")
+                 .ToListAsync();
+
+            return View(patientAppointments);
+        }
+
+        public async Task <IActionResult> MedicalRegisters()
+        {
+            var medicalRegisters = await _dbcontext.MedicalRegisters.ToListAsync();
+            return View(medicalRegisters);
+        }
+    }
+}
