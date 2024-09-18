3662c650c1db343dd175242a43ece2a77a43c413
Alan Coiradas <coiradasalan@gmail.com>
2024-09-18 11:55:21 -0300

    Creación de viewmodel para registro de doctores

---
+++ HealthManager/Models/DTO/DoctorRegistrationViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthManager.Models.DTO
+{
+    public class DoctorRegistrationViewModel
+    {
+        [Required, MaxLength(100), MinLength(2)]
+        public string Name { get; set; }
+        [Required, MaxLength(100), MinLength(2)]
+        public string? Surname { get; set; }
+        [Required, MaxLength(100), MinLength(2)]
+        public string Specialty { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required, Compare("Password")]
+        public string ConfirmPassword {  get; set; }
+        [Required]  
+        public TimeOnly WorkingHoursStart { get; set; }
+        [Required]
+        public TimeOnly WorkingHoursEnd { get; set; }
+        [Required]
+        public TimeOnly ConsultationDuration { get; set; }
+        [Required]
+        public bool Monday { get; set; }
+
+        [Required]
+        public bool Tuesday { get; set; }
+
+        [Required]
+        public bool Wednesday { get; set; }
+
+        [Required]
+        public bool Thursday { get; set; }
+
+        [Required]
+        public bool Friday { get; set; }
+
+        [Required]
+        public bool Saturday { get; set; }
+
+        [Required]
+        public bool Sunday { get; set; }
+    }
+}
