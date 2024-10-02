9c73c221c03b52d2c14b0f24e6af262ca59e73bb
Alan Coiradas <coiradasalan@gmail.com>
2024-10-02 15:21:46 -0300

    AppointmentsService: Actualización de método CreateAppointments + creación de método CheckForExistingRegisters
    - Se actualizó la lógica del método que crea los turnos médicos. Se implementó un sistema de transacciones para que se garantice que los registros se guarden en la base de datos si todas las operaciones se ejecutan exitosamente.
    
    - Primero, mediante una query se obtiene la lista de doctores , luego se hace uso de un inner join que mediante la clave primaria DoctorId, se enlaza a las tablas AppointmentInfo y WorkingDays (Esto para obtener la información de horarios de atención, duración de consulta y días de atención). Luego se obtiene una lista de propiedades que corresponden a los días de la semana. A continuación, se usa un for para recorrer la lista de doctores. Por cada doctor, se obtienen los días laborales del mismo. Para después recorrer mediante un for los días del mes obtenido.  Luego, se recorre la lista de días de la semana. se obtiene el día correspondiente al for de los días del mes, si el día es uno en los cuales atiende el médico, se recorre desde el horario de comienzo de atención hasta el horario de finalización, aumentando el valor del condicional en el tiempo de duración de cada consulta (este bucle se ejecuta hasta que el tiempo sea mayor o igual al tiempo de finalización de consulta del médico), se crean los turnos médicos en base a estas condiciones y se sigue así por cada doctor, iterando los días del mes presente y comprobando que cada día es un día en el que el médico atiende, para luego crear los turnos médicos para dicho día y que el paciente pueda reservarlos.
    
    - El método CheckForExistingRegisters comprueba si hay turnos médicos para los especialistas dentro del mes presente, y retorna una respuesta con una propiedad booleana y un mensaje que indica el resultado de la operación.

--- HealthManager/Services/Appointments/AppointmentsService.cs
+++ HealthManager/Services/Appointments/AppointmentsService.cs
@@ -1,6 +1,10 @@
 ﻿using HealthManager.Models;
+using HealthManager.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Numerics;
+using System.Reflection;
 
 namespace HealthManager.Services.Appointments
 {
@@ -13,37 +17,111 @@ namespace HealthManager.Services.Appointments
             _context = context;
         }
 
-        public async Task<IActionResult> CreateAppointments()
+        public async Task<MethodResponse> CreateAppointments()
         {
-            int currentYear = DateTime.Now.Year;
-            List<Doctor> doctorsCollection = _context.Doctors
-                .Include(d => d.AppointmentInfos)
-                .Include(d => d.WorkingDays)
-                .ToList();
-            foreach(Doctor doctor in doctorsCollection)
+            using var databaseContext = new HealthManagerContext();
+            using var transaction = await databaseContext.Database.BeginTransactionAsync();
+            try
             {
-                int currentMonth = DateTime.Now.Month;
-                int daysInMonth = System.DateTime.DaysInMonth(currentYear, currentMonth);
+                int currentYear = DateTime.Now.Year;
 
-                List<WorkingDay> doctorWorkingDays = doctor.WorkingDays.ToList();
-                for (int i=1; i<=daysInMonth; i++)
+                var query = from Doctor in _context.Doctors
+                            join AppointmentInfo in _context.AppointmentInfos
+                            on Doctor.DoctorId equals AppointmentInfo.DoctorId
+                            join workingDay in _context.WorkingDays
+                            on Doctor.DoctorId equals workingDay.DoctorId
+                            select new
+                            {
+                                Doctor.DoctorId,
+                                DoctorName = Doctor.Name,
+                                AppointmentStart = AppointmentInfo.WorkingHoursStart,
+                                AppointmentEnd = AppointmentInfo.WorkingHoursEnd,
+                                ConsultDuration = AppointmentInfo.ConsultationDuration,
+                                workingDay
+                            };
+
+                var doctorsList = await query.ToListAsync();
+
+                Type workingDayType = typeof(HealthManager.Models.WorkingDay);
+
+                PropertyInfo[] properties = workingDayType.GetProperties();
+
+                var dayProperties = properties.Where(prop =>
+                    prop.Name == "Monday" || prop.Name == "Tuesday" || prop.Name == "Wednesday" ||
+                    prop.Name == "Thursday" || prop.Name == "Friday" || prop.Name == "Saturday" ||
+                    prop.Name == "Sunday"
+                );
+
+                foreach (var doctor in doctorsList)
                 {
-                    int currentDay = i;
+                    int currentMonth = DateTime.Now.Month;
+                    int daysInMonth = System.DateTime.DaysInMonth(currentYear, currentMonth);
 
+
+                    var doctorWorkingDays = doctor.workingDay;
+                    for (int i = 1; i <= daysInMonth; i++)
+                    {
+                        int currentDay = i;
+                        foreach (var day in dayProperties)
+                        {
+                            DateTime auxiDay = new DateTime(currentYear, currentMonth, i);
+                            string dayName = auxiDay.ToString("dddd");
+                            if (dayName.Equals(day))
+                            {
+                                var doctorConsultationStart = doctor.AppointmentStart;
+                                var doctorConsultationEnd = doctor.AppointmentEnd;
+                                var doctorConsultDuration = doctor.ConsultDuration;
+
+                                var hourCount = Convert.ToDateTime(doctorConsultationStart);
+                                var limitHourAuxi = Convert.ToDateTime(doctorConsultationEnd);
+
+                                var durationAuxi = doctorConsultDuration.Minute + doctorConsultDuration.Hour * 60;
+
+                                while (hourCount < limitHourAuxi)
+                                {
+                                    Appointment newAppointment = new Appointment
+                                    {
+                                        AppointmentId = new Guid(),
+                                        AppointmentDate = new DateOnly(currentYear, currentMonth, currentDay),
+                                        AppointmentHour = TimeOnly.FromDateTime(hourCount),
+                                        Status = "Available",
+                                        PatientId = 0,
+                                        DoctorId = doctor.DoctorId,
+
+                                    };
+                                    await _context.Appointments.AddAsync(newAppointment);
+                                    await databaseContext.SaveChangesAsync();
+
+                                    hourCount = hourCount.AddMinutes(durationAuxi);
+                                }
+                            }
+                        }
+                    }
                 }
+                await transaction.CommitAsync();
+                return new MethodResponse { Success = true, Message = "Appointments created successfully" };
             }
+            catch (Exception)
+            {
+
+                await transaction.RollbackAsync();
+                return new MethodResponse { Success = false, Message = "Appointments could not be created" };
+            }
+             
         }
 
-        public async Task<Appointment> GetAvailableAppointments()
+        public async Task<MethodResponse> CheckForExistingRegisters()
         {
-            int month = 1;
             int currentMonth = DateTime.Now.Month;
-            int currentYear = DateTime.Now.Year;
-            List<Appointment> appointments = _context.Appointments
-                .Where(x => x.AppointmentDate.Month == currentMonth && x.AppointmentDate.Year == currentYear)
-                .ToList();
-
-            throw new NotImplementedException();
+            var existingRegisters = await _context.Appointments.Where(a => a.AppointmentDate.Month == currentMonth).ToListAsync();
+            if (existingRegisters.Count > 0)
+            {
+                return new MethodResponse{Success = true, Message="There are registers corresponding to the present month in the database"};
+            }
+            else
+            {
+                return new MethodResponse { Success = false, Message="There are no registers corresponding to actual month in the database. It is recommended to create new appointments in the database"};
+            }
         }
 
 
