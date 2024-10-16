bb471ca8e3332836a8c6917aaeee2c37aba3b709
Alan Coiradas <coiradasalan@gmail.com>
2024-10-16 13:40:51 -0300

    PatientViewModel: Modificada la estructura del modelo dejando las propiedades necesarias para la verificación de los datos del formulario de registro.

--- HealthManager/Models/DTO/PatientViewModel.cs
+++ HealthManager/Models/DTO/PatientViewModel.cs
@@ -2,9 +2,22 @@
 
 namespace HealthManager.Models.DTO
 {
-    public class PatientViewModel: Patient
+    public class PatientViewModel
     {
+        public string Name { get; set; } = null!;
+        [MaxLength(100)]
+        [MinLength(1)]
+        public string Surname { get; set; } = null!;
+        [Required]
+        public DateOnly Birthdate { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required, MinLength(12), RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$")]
+        public string Password { get; set; } = null!;
+        [Required]
+        public int Dni { get; set; }
         [Required, Compare("Password"), MinLength(12), RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$")]
-        public string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; } = null!;
     }
 }
