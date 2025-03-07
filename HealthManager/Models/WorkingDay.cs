c490fd952636da20b724c35adb4f3b6faf46f3fb
Alan Coiradas <coiradasalan@gmail.com>
2025-03-07 16:22:17 -0300

    WorkingDays: Actualización de modelo

--- HealthManager/Models/WorkingDay.cs
+++ HealthManager/Models/WorkingDay.cs
@@ -6,32 +6,23 @@ namespace HealthManager.Models;
 
 public partial class WorkingDay
 {
-    public int WorkingDaysId { get; set; }
+    public int Id { get; set; }
 
     public int? DoctorId { get; set; }
-
     [Required]
-    public bool? Monday { get; set; }
-
+    public bool Sunday { get; set; }
     [Required]
-    public bool? Tuesday { get; set; }
-
+    public bool Monday { get; set; }
     [Required]
-    public bool? Wednesday { get; set; }
-
+    public bool Tuesday { get; set; }
     [Required]
-    public bool? Thursday { get; set; }
-
+    public bool Wednesday { get; set; }
     [Required]
-    public bool? Friday { get; set; }
-
+    public bool Thursday { get; set; }
     [Required]
-    public bool? Saturday { get; set; }
-
+    public bool Friday { get; set; }
     [Required]
-    public bool? Sunday { get; set; }
-
-    public virtual ICollection<AppointmentInfo> AppointmentInfos { get; set; } = new List<AppointmentInfo>();
+    public bool Saturday { get; set; }
 
     public virtual Doctor? Doctor { get; set; }
 }
