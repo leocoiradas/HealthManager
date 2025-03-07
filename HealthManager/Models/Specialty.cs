c8e526746bd130b4decba97ea527397e66c3f3b4
Alan Coiradas <coiradasalan@gmail.com>
2025-03-07 16:27:38 -0300

    Creación de clase Specialty para almacenar todas las especialidades médicas en dicha tabla

---
+++ HealthManager/Models/Specialty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HealthManager.Models;
+
+public partial class Specialty
+{
+    public int SpecialtyId { get; set; }
+
+    public string SpecialtyName { get; set; } = null!;
+
+    public virtual ICollection<Doctor> Doctors { get; set; } = new List<Doctor>();
+}
