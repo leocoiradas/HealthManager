6f37e3226b6e97eec7c254d6188a7caec67aa8b7
Alan Coiradas <coiradasalan@gmail.com>
2025-03-18 17:39:13 -0300

    Creada clase para almacenar métodos que se ejecutan durante varias etapas del ciclo de vida de la aplicación.

---
+++ HealthManager/Services/Appointments/AppointmentsLifeCicle.cs
@@ -0,0 +1,48 @@
+﻿
+namespace HealthManager.Services.Appointments
+{
+    public class AppointmentsLifeCicle : IHostedLifecycleService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        public AppointmentsLifeCicle(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            //await _appointmentsService.CheckAndCreateAppointments();
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var appointmentsService = scope.ServiceProvider.GetRequiredService<IAppointments>();
+                await appointmentsService.CheckAndCreateAppointments();
+            }
+
+        }
+
+        public Task StartedAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StartingAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StoppedAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StoppingAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
