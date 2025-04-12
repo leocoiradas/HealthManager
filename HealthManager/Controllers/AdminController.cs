aefb6b0c9d571d7f6039350f9ec89be850eb6cb4
Alan Coiradas <coiradasalan@gmail.com>
2025-04-12 16:19:38 -0300

    Agregado método de creación de perfil de doctor proveniente de la clase DoctorController

--- HealthManager/Controllers/AdminController.cs
+++ HealthManager/Controllers/AdminController.cs
@@ -23,28 +23,70 @@ namespace HealthManager.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateDoctor(DoctorRegistrationViewModel doctorRequest)
+        {
+
+            using var transaction = await _dbcontext.Database.BeginTransactionAsync();
             try
             {
                 var existingRegisters = await _appointmentsService.CheckForExistingRegisters();
+                Specialty newDoctorSpecialty = (Specialty)_dbcontext.Specialties.Where(x => x.SpecialtyId == doctorRequest.Specialty);
+                Doctor newDoctor = new Doctor
+                {
+                    Name = doctorRequest.Name,
+                    Surname = doctorRequest.Surname,
+                    Specialty = doctorRequest.Specialty,
+                    Email = doctorRequest.Email,
+                    Password = BCrypt.Net.BCrypt.HashPassword(doctorRequest.Password),
+                };
+                await _dbcontext.Doctors.AddAsync(newDoctor);
+                await _dbcontext.SaveChangesAsync();
 
                 if (existingRegisters.Success.Equals(true))
+                WorkingDay newDoctorSchedule = new WorkingDay
                 {
                     return Json(new {success = false, message="There are already appointments for this month" });
                 }
                 else
+                    DoctorId = newDoctor.DoctorId,
+                    Monday = doctorRequest.Monday,
+                    Tuesday = doctorRequest.Tuesday,
+                    Wednesday = doctorRequest.Wednesday,
+                    Thursday = doctorRequest.Thursday,
+                    Friday = doctorRequest.Friday,
+                    Saturday = doctorRequest.Saturday,
+                    Sunday = doctorRequest.Sunday,
+                };
+                await _dbcontext.WorkingDays.AddAsync(newDoctorSchedule);
+                await _dbcontext.SaveChangesAsync();
+
+                DoctorShift newAppointmentInfo = new DoctorShift
                 {
                     await _appointmentsService.CreateAppointments();
                     return Json(new { success = true, message = "Appointments were successfully created." });
                 }
                 
                 
+                    DoctorId = newDoctor.DoctorId,
+                    ShiftStart = doctorRequest.WorkingHoursStart,
+                    ShiftEnd = doctorRequest.WorkingHoursEnd,
+                    ConsultDuration = doctorRequest.ConsultationDuration,
+                };
+
+                await _dbcontext.DoctorShifts.AddAsync(newAppointmentInfo);
+                await _dbcontext.SaveChangesAsync();
+
+                await transaction.CommitAsync();
+                return View();
             }
             catch (Exception error)
+            catch (Exception)
             {
                 return Json(new {success= false, message = error });
-                
+                await transaction.RollbackAsync();
+                return View(doctorRequest);
             }
-           
         }
     }
 }
