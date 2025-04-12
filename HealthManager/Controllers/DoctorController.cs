95f2acb6deb01d6f396ba29f0900ea8c143ced9a
Alan Coiradas <coiradasalan@gmail.com>
2025-04-12 16:20:33 -0300

    Eliminado método CreateDoctor de la clase DoctorController

--- HealthManager/Controllers/DoctorController.cs
+++ HealthManager/Controllers/DoctorController.cs
@@ -31,66 +31,6 @@ namespace HealthManager.Controllers
             return View(patientList);
         }
 
-        [HttpGet]
-        public IActionResult CreateDoctor()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task <IActionResult> CreateDoctor(DoctorRegistrationViewModel doctorRequest)
-        {
-            
-            using var transaction = await _dbcontext.Database.BeginTransactionAsync();
-            try
-            {
-                Specialty newDoctorSpecialty = (Specialty)_dbcontext.Specialties.Where(x => x.SpecialtyId == doctorRequest.Specialty);
-                Doctor newDoctor = new Doctor
-                {
-                    Name = doctorRequest.Name,
-                    Surname = doctorRequest.Surname,
-                    Specialty = doctorRequest.Specialty,
-                    Email = doctorRequest.Email,
-                    Password = BCrypt.Net.BCrypt.HashPassword(doctorRequest.Password),
-                };
-                await _dbcontext.Doctors.AddAsync(newDoctor);
-                await _dbcontext.SaveChangesAsync();
-
-                WorkingDay newDoctorSchedule = new WorkingDay
-                {
-                    DoctorId = newDoctor.DoctorId,
-                    Monday = doctorRequest.Monday,
-                    Tuesday = doctorRequest.Tuesday,
-                    Wednesday = doctorRequest.Wednesday,
-                    Thursday = doctorRequest.Thursday,
-                    Friday = doctorRequest.Friday,
-                    Saturday = doctorRequest.Saturday,
-                    Sunday = doctorRequest.Sunday,
-                };
-                await _dbcontext.WorkingDays.AddAsync(newDoctorSchedule);
-                await _dbcontext.SaveChangesAsync();
-
-                DoctorShift newAppointmentInfo = new DoctorShift
-                {
-                    DoctorId = newDoctor.DoctorId,
-                    ShiftStart = doctorRequest.WorkingHoursStart,
-                    ShiftEnd = doctorRequest.WorkingHoursEnd,
-                    ConsultDuration = doctorRequest.ConsultationDuration,
-                };
-
-                await _dbcontext.DoctorShifts.AddAsync(newAppointmentInfo);
-                await _dbcontext.SaveChangesAsync();
-
-                await transaction.CommitAsync();
-            }
-            catch (Exception)
-            {
-
-                await transaction.RollbackAsync();
-            }
-            
-            return View();
-        }
-
         public async Task<JsonResult> GetDoctorsBySpecialty(int specialty)
         {
             var doctorsBySpecialty = await _dbcontext.Doctors.Where(d => d.Specialty == specialty)
