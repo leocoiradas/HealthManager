55f7ac0cf7fb0b217df3265423550137a1f9b684
Alan Coiradas <coiradasalan@gmail.com>
2025-04-17 18:13:35 -0300

    Actualizado contexto de la base de datos

--- HealthManager/Models/HealthManagerContext.cs
+++ HealthManager/Models/HealthManagerContext.cs
@@ -15,6 +15,8 @@ public partial class HealthManagerContext : DbContext
     {
     }
 
+    public virtual DbSet<Admin> Admins { get; set; }
+
     public virtual DbSet<Appointment> Appointments { get; set; }
 
     public virtual DbSet<Doctor> Doctors { get; set; }
@@ -30,12 +32,35 @@ public partial class HealthManagerContext : DbContext
     public virtual DbSet<WorkingDay> WorkingDays { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Admin>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Admin__3214EC07C6600284");
+
+            entity.ToTable("Admin");
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.Role)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasDefaultValue("Admin");
+            entity.Property(e => e.Surname)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
         modelBuilder.Entity<Appointment>(entity =>
         {
-            entity.HasKey(e => e.AppointmentId).HasName("PK__Appointm__8ECDFCC26BA293D4");
+            entity.HasKey(e => e.AppointmentId).HasName("PK__Appointm__8ECDFCC2133FBFA5");
 
             entity.Property(e => e.AppointmentId).HasDefaultValueSql("(newid())");
             entity.Property(e => e.Status)
@@ -44,16 +69,17 @@ public partial class HealthManagerContext : DbContext
 
             entity.HasOne(d => d.Doctor).WithMany(p => p.Appointments)
                 .HasForeignKey(d => d.DoctorId)
-                .HasConstraintName("FK__Appointme__Docto__0A9D95DB");
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Appointme__Docto__6AEFE058");
 
             entity.HasOne(d => d.Patient).WithMany(p => p.Appointments)
                 .HasForeignKey(d => d.PatientId)
-                .HasConstraintName("FK__Appointme__Patie__09A971A2");
+                .HasConstraintName("FK__Appointme__Patie__69FBBC1F");
         });
 
         modelBuilder.Entity<Doctor>(entity =>
         {
-            entity.HasKey(e => e.DoctorId).HasName("PK__Doctors__2DC00EBF1E844E37");
+            entity.HasKey(e => e.DoctorId).HasName("PK__Doctors__2DC00EBF17F40596");
 
             entity.Property(e => e.Email)
                 .HasMaxLength(100)
@@ -75,24 +101,24 @@ public partial class HealthManagerContext : DbContext
             entity.HasOne(d => d.SpecialtyNavigation).WithMany(p => p.Doctors)
                 .HasForeignKey(d => d.Specialty)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Doctors__Special__03F0984C");
+                .HasConstraintName("FK__Doctors__Special__42E1EEFE");
         });
 
         modelBuilder.Entity<DoctorShift>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__DoctorSh__3214EC0735567BEA");
+            entity.HasKey(e => e.Id).HasName("PK__DoctorSh__3214EC07B8C4C10C");
 
             entity.ToTable("DoctorShift");
 
             entity.HasOne(d => d.Doctor).WithMany(p => p.DoctorShifts)
                 .HasForeignKey(d => d.DoctorId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__DoctorShi__Docto__114A936A");
+                .HasConstraintName("FK__DoctorShi__Docto__719CDDE7");
         });
 
         modelBuilder.Entity<MedicalRecord>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__MedicalR__3214EC072818A6B5");
+            entity.HasKey(e => e.Id).HasName("PK__MedicalR__3214EC07FC954C77");
 
             entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
             entity.Property(e => e.Date)
@@ -110,22 +136,22 @@ public partial class HealthManagerContext : DbContext
 
             entity.HasOne(d => d.Appointment).WithMany(p => p.MedicalRecords)
                 .HasForeignKey(d => d.AppointmentId)
-                .HasConstraintName("FK__MedicalRe__Appoi__1AD3FDA4");
+                .HasConstraintName("FK__MedicalRe__Appoi__756D6ECB");
 
             entity.HasOne(d => d.Doctor).WithMany(p => p.MedicalRecords)
                 .HasForeignKey(d => d.DoctorId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__MedicalRe__Docto__1BC821DD");
+                .HasConstraintName("FK__MedicalRe__Docto__76619304");
 
             entity.HasOne(d => d.Patient).WithMany(p => p.MedicalRecords)
                 .HasForeignKey(d => d.PatientId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__MedicalRe__Patie__1CBC4616");
+                .HasConstraintName("FK__MedicalRe__Patie__7755B73D");
         });
 
         modelBuilder.Entity<Patient>(entity =>
         {
-            entity.HasKey(e => e.PatientId).HasName("PK__Patients__970EC3664C1B74A1");
+            entity.HasKey(e => e.PatientId).HasName("PK__Patients__970EC3667AD5ABCB");
 
             entity.Property(e => e.Email)
                 .HasMaxLength(30)
@@ -155,7 +181,7 @@ public partial class HealthManagerContext : DbContext
 
         modelBuilder.Entity<Specialty>(entity =>
         {
-            entity.HasKey(e => e.SpecialtyId).HasName("PK__Specialt__D768F6A84B595E72");
+            entity.HasKey(e => e.SpecialtyId).HasName("PK__Specialt__D768F6A8E0D0F780");
 
             entity.Property(e => e.SpecialtyName)
                 .HasMaxLength(50)
@@ -164,11 +190,11 @@ public partial class HealthManagerContext : DbContext
 
         modelBuilder.Entity<WorkingDay>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__WorkingD__3214EC078CE04C0B");
+            entity.HasKey(e => e.Id).HasName("PK__WorkingD__3214EC07ADD1DDE2");
 
             entity.HasOne(d => d.Doctor).WithMany(p => p.WorkingDays)
                 .HasForeignKey(d => d.DoctorId)
-                .HasConstraintName("FK__WorkingDa__Docto__0E6E26BF");
+                .HasConstraintName("FK__WorkingDa__Docto__6EC0713C");
         });
 
         OnModelCreatingPartial(modelBuilder);
