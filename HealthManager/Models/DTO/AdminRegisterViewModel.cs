70a0cb09483a6f5845b1499f33f26775ef8cebf2
Alan Coiradas <coiradasalan@gmail.com>
2025-04-27 20:06:59 -0300

    Creado viewmodel para el registro de Admin.

---
+++ HealthManager/Models/DTO/AdminRegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthManager.Models.DTO
+{
+    public class AdminRegisterViewModel
+    {
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MinLength(12)]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-+]).{12,}$")]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; } = null!;
+
+        public string Role { get; set; } = "Admin";
+    }
+}
