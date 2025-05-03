c7d8ab4ca9dc97d5da724c595ad03e0ae21f289f
Alan Coiradas <coiradasalan@gmail.com>
2025-05-03 20:58:30 -0300

    Comentado código de autenticación por cookie

--- HealthManager/Program.cs
+++ HealthManager/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,7 +37,7 @@ builder.Services.AddScoped<IAppointments, AppointmentsService>();
 
 //Configuración de autenticación
 
-/*var tokenKey = builder.Configuration.GetSection("JWT").GetSection("secret-key").ToString();
+var tokenKey = builder.Configuration.GetSection("JWT").GetSection("secret-key").ToString();
 
 builder.Services.AddAuthentication(x =>
 {
@@ -70,15 +71,15 @@ builder.Services.AddAuthentication(x =>
             return Task.CompletedTask;
         }
     };
-});*/
+});
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+/*builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
 .AddCookie(option =>
 {
     option.LoginPath = "/Authorize/Login";
     option.ExpireTimeSpan = TimeSpan.FromDays(7);
     option.AccessDeniedPath = "/Authorize/Login";
-});
+});*/
 
 var app = builder.Build();
 
