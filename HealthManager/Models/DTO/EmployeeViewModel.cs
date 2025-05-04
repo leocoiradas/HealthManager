f92778c7eb6b0cd995e3345efb39b90844dfc0a1
Alan Coiradas <coiradasalan@gmail.com>
2025-05-03 23:05:33 -0300

    Creado EmployeeViewModel.
    - Creado Viewmodel con propiedades para almacenar la lista de empleados (admin y doctor) presentes en la db.

---
+++ HealthManager/Models/DTO/EmployeeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthManager.Models.DTO
+{
+    public class EmployeeViewModel
+    {
+        public List<Admin> AdminList { get; set; } = null!;
+
+        public List<Doctor> DoctorList { get; set; } = null!;
+    }
+}
