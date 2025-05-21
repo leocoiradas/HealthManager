fff8a7e46de7759b795c447e83abee54e6d5b7d3
Alan Coiradas <coiradasalan@gmail.com>
2025-05-21 19:05:52 -0300

    Añadidos imports de Autorización y manejo de claims

--- HealthManager/Controllers/AppointmentController.cs
+++ HealthManager/Controllers/AppointmentController.cs
@@ -1,10 +1,12 @@
 ﻿using HealthManager.Models;
 using HealthManager.Models.DTO;
 using HealthManager.Services.Appointments;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Security.Claims;
 
 namespace HealthManager.Controllers
 {
