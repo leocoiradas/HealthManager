57d46aee105758ac074de133a020b3f8ddb85929
Alan Coiradas <coiradasalan@gmail.com>
2025-06-23 14:03:26 -0300

    Añadidos mensajes de error de validación en el VM de Paciente.
    - Adicionalmente se le dio formato a los atributos de error en el ViewModel.

--- HealthManager/Models/DTO/PatientViewModel.cs
+++ HealthManager/Models/DTO/PatientViewModel.cs
@@ -4,26 +4,35 @@ namespace HealthManager.Models.DTO
 {
     public class PatientViewModel
     {
+        [Required(ErrorMessage = "This field is required.")]
+        [MaxLength(100)]
+        [MinLength(1)]
         public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "This field is required.")]
         [MaxLength(100)]
         [MinLength(1)]
         public string Surname { get; set; } = null!;
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         public DateOnly Birthdate { get; set; }
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         [EmailAddress]
         public string Email { get; set; } = null!;
-        [Required, MinLength(12), RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-+]).{12,}$")]
+        [Required(ErrorMessage = "This field is required.")]
+        [MinLength(12)]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-+]).{12,}$")]
         public string Password { get; set; } = null!;
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         public int Dni { get; set; }
-        [Required, Compare("Password"), MinLength(12), RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*+-]).{12,}$")]
+        [Required(ErrorMessage = "This field is required.")]
+        [Compare("Password", ErrorMessage ="Passwords do not match.")]
+        [MinLength(12)]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*+-]).{12,}$")]
         public string ConfirmPassword { get; set; } = null!;
         public long? PhoneNumber { get; set; }
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         [MaxLength(1)]
         public string? Gender { get; set; }
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         [MaxLength(1)]
         public string? Sex { get; set; }
     }
