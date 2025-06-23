c9fcf94c837bf5744d2cdc878868e8e798fccc64
Alan Coiradas <coiradasalan@gmail.com>
2025-06-23 10:30:00 -0300

    Editados mensajes de error del VM de admin.

--- HealthManager/Models/DTO/AdminRegisterViewModel.cs
+++ HealthManager/Models/DTO/AdminRegisterViewModel.cs
@@ -5,25 +5,25 @@ namespace HealthManager.Models.DTO
     public class AdminRegisterViewModel
     {
 
-        [Required]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "This field is required")]
+        [MaxLength(50, ErrorMessage ="The maximum of characters for this field is 50")]
         public string FirstName { get; set; } = null!;
 
-        [Required]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "This field is required")]
+        [MaxLength(50, ErrorMessage = "The maximum of characters for this field is 50")]
         public string LastName { get; set; } = null!;
 
-        [Required]
+        [Required(ErrorMessage = "This field is required")]
         [EmailAddress]
         public string Email { get; set; } = null!;
 
-        [Required]
-        [MinLength(12)]
+        [Required(ErrorMessage = "This field is required")]
+        [MinLength(12, ErrorMessage = "The minimum characters for this field is 12")]
         [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-+]).{12,}$")]
         public string Password { get; set; } = null!;
 
-        [Required]
-        [Compare("Password")]
+        [Required(ErrorMessage = "This field is required")]
+        [Compare("Password", ErrorMessage ="The passwords must be the same")]
         public string ConfirmPassword { get; set; } = null!;
 
         public string Role { get; set; } = "Admin";
