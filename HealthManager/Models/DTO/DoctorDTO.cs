4d6efe51955fe8914db2326d8476b8435525cafc
Alan Coiradas <coiradasalan@gmail.com>
2025-07-04 14:01:20 -0300

    Creado DTO de Doctor.

---
+++ HealthManager/Models/DTO/DoctorDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthManager.Models.DTO
+{
+    public class DoctorDTO
+    {
+        public Doctor Doctor { get; set; } = null!;
+        public WorkingDay WorkingDay { get; set; } = null!;
+        public DoctorShift DoctorShift { get; set; } = null!;
+    }
+}
