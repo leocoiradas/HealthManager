06fca817233ffd9949002017c241190b8585fefe
Alan Coiradas <coiradasalan@gmail.com>
2025-10-20 17:50:28 -0300

    Adición y remoción de dependencias usadas en la clase

--- HealthManager/Controllers/AppointmentController.cs
+++ HealthManager/Controllers/AppointmentController.cs
@@ -1,12 +1,12 @@
 ﻿using HealthManager.Models;
 using HealthManager.Models.DTO;
 using HealthManager.Services.Appointments;
+using HealthManager.Services.Mail;
+using HealthManager.Services.PDF.AppointmentReceipt;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Security.Claims;
 
 namespace HealthManager.Controllers
 {
