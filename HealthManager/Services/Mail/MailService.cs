ae30793a4bd5a277a989d95f929e7117033d3d80
Alan Coiradas <coiradasalan@gmail.com>
2025-10-20 17:45:43 -0300

    Creada clase MailService
    - Se creó dicho service donde se implementa la interfaz IMailService. Se implementa el método SendAppointmentConfirmationEmail que recibe un objeto de la clase MailDTO y un array de bytes (este es el tipo de dato que retorna el método de generación de PDF, por lo que este parametro, corresponde a ese PDF en este formato), luego, en base a los datos ingresados, envía un mail al destinatario con la confirmación de su turno médico en formato PDF y usando la plantilla HTML creada anteriormente.

---
+++ HealthManager/Services/Mail/MailService.cs
@@ -0,0 +1,59 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http.HttpResults;
+using MimeKit;
+
+namespace HealthManager.Services.Mail
+{
+    public class MailService : IMailService
+    {
+        private IConfiguration _config;
+        public MailService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void SendAppointmentConfirmationMail(MailDTO mailProfile, byte[] pdfBytes)
+        {
+            string rutaHtml = Path.Combine(Directory.GetCurrentDirectory(), "Services", "Mail", "mailprofile.html");
+            string htmlFile = File.ReadAllText(rutaHtml);
+
+            var mailTemplate = new MimeMessage();
+
+            mailTemplate.From.Add(new MailboxAddress("HealthManager Project", _config.GetSection("Email:Username").Value));
+            mailTemplate.To.Add(new MailboxAddress(mailProfile.DestinataryName, mailProfile.DestinataryMail));
+            mailTemplate.Subject = mailProfile.MailSubject;
+            mailTemplate.Body = new TextPart("plain")
+            {
+                Text = ""
+            };
+
+            var builder = new BodyBuilder
+            {
+                HtmlBody = htmlFile
+            };
+
+            
+
+            builder.Attachments.Add(
+            "Comprobante.pdf",
+            pdfBytes,
+            new ContentType("application", "pdf")
+        );
+
+            mailTemplate.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(
+                _config.GetSection("Email:Host").Value,
+                Convert.ToInt32(_config.GetSection("Email:Port").Value),
+                SecureSocketOptions.StartTls
+            );
+
+            smtp.Authenticate(_config.GetSection("Email:UserName").Value, _config.GetSection("Email:PassWord").Value);
+
+            smtp.Send(mailTemplate);
+
+            smtp.Disconnect(true);
+        }
+    }
+}
