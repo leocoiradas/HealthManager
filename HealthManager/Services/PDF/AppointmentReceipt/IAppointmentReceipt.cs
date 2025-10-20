3901a691a7a55ff046bf7d2813718d47e5993553
Alan Coiradas <coiradasalan@gmail.com>
2025-10-20 16:35:51 -0300

    Creada interfaz de comprobantes de turnos médicos

---
+++ HealthManager/Services/PDF/AppointmentReceipt/IAppointmentReceipt.cs
@@ -0,0 +1,10 @@
+﻿using HealthManager.Models;
+using HealthManager.Models.DTO;
+
+namespace HealthManager.Services.PDF.AppointmentReceipt
+{
+    public interface IAppointmentReceipt
+    {
+        public byte[] CreateAppointmentReceipt(AppointmentDataPDFDTO appointment);
+    }
+}
