060613febc5a731df078a70470823bdf0d1db738
Alan Coiradas <coiradasalan@gmail.com>
2025-12-18 16:37:03 -0300

    Creada clase AppointmentTests
    - Creada clase para almacenar los tests correspondientes al sistema de turnos médicos

---
+++ HealthManager.IntegrationTests/ControllerTests/AppointmentTests/AppointmentTests.cs
@@ -0,0 +1,33 @@
+﻿using HealthManager.Models;
+using HealthManager.Models.DTO;
+using HealthManager.Services.Authentication;
+using HealthManagerIntegrationTests;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthManagerIntegrationTests.ControllerTests.AppointmentTests
+{
+    public class AppointmentTests : IClassFixture<CustomWebApplicationEnv>
+    {
+        private HttpClient _httpClient;
+        private readonly HealthManagerContext _dbContext;
+        private readonly IJWTService _tokenService;
+        public AppointmentTests(CustomWebApplicationEnv custom)
+        {
+            _httpClient = custom.CreateClient();
+            var scope = custom.Services.CreateScope();
+            _dbContext = scope.ServiceProvider.GetRequiredService<HealthManagerContext>();
+            _tokenService = scope.ServiceProvider.GetRequiredService<IJWTService>();
+        }
+
+    }
+}
