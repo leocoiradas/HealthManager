c321e3e9ff4b23b6aa68c4acf10f82c800022c9f
Alan Coiradas <coiradasalan@gmail.com>
2025-12-19 12:52:17 -0300

    Creada clase DoctorTests
    - Creada clase para almacenar los test correspondientes a los métodos de doctores.

---
+++ HealthManager.IntegrationTests/ControllerTests/DoctorTests/DoctorTests.cs
@@ -0,0 +1,41 @@
+﻿using Docker.DotNet.Models;
+using HealthManager.Models;
+using HealthManager.Models.DTO;
+using HealthManager.Services.Appointments;
+using HealthManager.Services.Authentication;
+using HealthManager.Services.JWTService;
+using HealthManagerIntegrationTests.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthManagerIntegrationTests.ControllerTests.DoctorTests
+{
+    public class DoctorTests : IClassFixture<CustomWebApplicationEnv>
+    {
+        private HttpClient _httpClient;
+        private readonly HealthManagerContext _dbContext;
+        private readonly IAppointments _appointmentsService;
+        private readonly IJWTService _jwtService;
+
+        public DoctorTests(CustomWebApplicationEnv custom)
+        {
+            _httpClient = custom.CreateClient(new Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryClientOptions 
+            {
+                AllowAutoRedirect = false
+            });
+            var scope = custom.Services.CreateScope();
+            _dbContext = scope.ServiceProvider.GetRequiredService<HealthManagerContext>();
+            _appointmentsService = scope.ServiceProvider.GetRequiredService<IAppointments>();
+            _jwtService = scope.ServiceProvider.GetRequiredService<IJWTService>();
+        }
+
+    }
+}
