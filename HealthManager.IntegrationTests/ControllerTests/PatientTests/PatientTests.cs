e2d81197606e96b21e90606216ce50888e033e27
Alan Coiradas <coiradasalan@gmail.com>
2025-12-19 14:21:01 -0300

    Creado test para verificar que la cancelación de turnos médicos falle si ya pasó la fecha del turno.
    - Se creó un test que verifica que la cancelación de turnos falle si la fecha del turno ya pasó, es decir, la fecha de solicitud de cancelación del turno es posterior a la fecha y hora del turno.

--- HealthManager.IntegrationTests/ControllerTests/PatientTests/PatientTests.cs
+++ HealthManager.IntegrationTests/ControllerTests/PatientTests/PatientTests.cs
@@ -106,5 +106,40 @@ namespace HealthManagerIntegrationTests.ControllerTests.PatientTests
             Assert.Equal(true, responseJson.Success);
         }
 
+        [Fact]
+        public async Task AppointmentCancellationFailsIfDateAlreadyPassed()
+        {
+            //Arrange
+            Patient patientTest = _dbContext.Patients.Where(x => x.PatientId == 1).FirstOrDefault();
+            string patientToken = _jwtService.GenerateToken(patientTest.Name, patientTest.Email, patientTest.Role, patientTest.PatientId);
+            _httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", patientToken);
+
+            DateOnly date = new DateOnly(2025, 12, 1);
+            TimeOnly time = new TimeOnly(10, 00);
+
+            Appointment appointment = await _dbContext.Appointments.Where(x => x.AppointmentDate == date && x.AppointmentHour == time).FirstOrDefaultAsync();
+
+            appointment.Status = "Reserved";
+            appointment.PatientId = patientTest.PatientId;
+
+            _dbContext.Update(appointment);
+
+            await _dbContext.SaveChangesAsync();
+
+
+            //Act
+
+            var auxiObject = new { appointmentId = appointment.AppointmentId.ToString() };
+            var formDataAppointmentId = AuxMethods.ConvertClassObjectToFormUrlEncoded(auxiObject);
+            var request = await _httpClient.PostAsync("/PatientDashboard/CancelAppointment", formDataAppointmentId);
+            MethodResponse responseJson = await request.Content.ReadFromJsonAsync<MethodResponse>();
+
+            //Assert
+
+            Assert.Equal(false, responseJson.Success);
+        }
+
+
     }
 }
