ac9c32f145860dec700d76eaddfbae5b17efef38
Alan Coiradas <coiradasalan@gmail.com>
2025-12-19 12:25:32 -0300

    Creada clase para almacenar tests del registro de usuarios.

---
+++ HealthManager.IntegrationTests/ControllerTests/AuthTests/RegisterTests.cs
@@ -0,0 +1,34 @@
+﻿using HealthManager.Models;
+using HealthManager.Models.DTO;
+using HealthManager.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using HealthManagerIntegrationTests.Helpers;
+
+namespace HealthManagerIntegrationTests.ControllerTests.AuthTests
+{
+    public class RegisterTests : IClassFixture<CustomWebApplicationEnv>
+    {
+        private HttpClient _httpClient;
+        private readonly HealthManagerContext _dbContext;
+        private readonly IJWTService _tokenService;
+
+        public RegisterTests(CustomWebApplicationEnv custom)
+        {
+            _httpClient = custom.CreateClient(new Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+            var scope = custom.Services.CreateScope();
+            _dbContext = scope.ServiceProvider.GetRequiredService<HealthManagerContext>();
+            _tokenService = scope.ServiceProvider.GetRequiredService<IJWTService>();
+        }
+
+    }
+}
