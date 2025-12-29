48e9de77c012f723c29151a733fd014961006d07
Alan Coiradas <coiradasalan@gmail.com>
2025-12-29 14:59:56 -0300

    AppEnv: Eliminadas inserciones a la db en InitializeAsync
    - Se eliminó el código que inserta registros en la base de datos con el objetivo de pasarlos a un archivo aparte para organizar el código.

--- HealthManager.IntegrationTests/CustomWebApplicationEnv.cs
+++ HealthManager.IntegrationTests/CustomWebApplicationEnv.cs
@@ -40,82 +40,9 @@ namespace HealthManagerIntegrationTests
 
             using var scope = Services.CreateScope();
 
-           
-                var db = scope.ServiceProvider.GetRequiredService<HealthManagerContext>();
-                var appointmentService = scope.ServiceProvider.GetRequiredService<IAppointments>();
-                db.Database.EnsureCreated();
-
-
-                if (!db.Specialties.Any())
-                {
-                    var neu = new Specialty { SpecialtyName = "Neurología" };
-                    await db.Specialties.AddAsync(neu);
-                    await db.SaveChangesAsync();
-                }
-                if (!db.Doctors.Any())
-                {
-                    var newDoctor = new Doctor
-                    {
-                        Name = "Mariano",
-                        Surname = "Benitez",
-                        Specialty = 1,
-                        Email = "marianobenitez@gmail.com",
-                        Password = BCrypt.Net.BCrypt.HashPassword("DefaultPass0213++"),
-                        Role ="Doctor"
-                    };
-                    await db.Doctors.AddAsync(newDoctor);
-                    await db.SaveChangesAsync();
-
-                    var wd = new WorkingDay
-                    {
-                        DoctorId = 1,
-                        Monday = true,
-                        Tuesday = true,
-                        Wednesday = true,
-                        Thursday = true,
-                        Friday = true,
-                        Saturday = true,
-                        Sunday = true
-                    };
-                    await db.WorkingDays.AddAsync(wd);
-                    await db.SaveChangesAsync();
-
-                    var doctorShift = new DoctorShift
-                    {
-                        DoctorId = 1,
-                        ShiftStart = new TimeOnly(10, 00),
-                        ShiftEnd = new TimeOnly(18, 30),
-                        ConsultDuration = new TimeOnly(00, 20)
-                    };
-                    await db.DoctorShifts.AddAsync(doctorShift);
-
-                    await db.SaveChangesAsync();
-
-
-                    var response = await appointmentService.CreateAppointmentsForAllDoctors();
 
 
 
-                }
-
-                if (!db.Patients.Any())
-                {
-                    Patient newPatient = new Patient
-                    {
-                        Name = "Alicia",
-                        Surname = "Rodriguez",
-                        Dni = 23456789,
-                        BirthDate = new DateOnly(1980, 11, 05),
-                        Gender = "F",
-                        Sex = "F",
-                        Email = "aliciarodriguez@gmail.com",
-                        Password = BCrypt.Net.BCrypt.HashPassword("Rodriguez02!!"),
-                        Role = "Patient"
-                    };
-                    await db.Patients.AddAsync(newPatient);
-                    await db.SaveChangesAsync();
-                }
-            
         }
 
         public async Task DisposeAsync()
@@ -139,9 +66,7 @@ namespace HealthManagerIntegrationTests
 
                 services.AddDbContext<HealthManagerContext>(options =>
                 options.UseSqlServer(_dbContainer.GetConnectionString()));
-
                 
-
             });
 
         }
